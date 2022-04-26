6b55a4919132d7020646df917940cd921f276d32
raidercl <103221925+raidercl@users.noreply.github.com>
2022-04-26 18:51:15 -0400

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,19 @@
+alert("Hola mundo");
+console.log("Hola mundo");
+ let variable = 45;
+ const constante = 10;
+ console.log(variable,constante);
+
+ function react() {
+    let obj = document.getElementById("react");
+    console.log(obj.value)
+
+    document.getElementById("text").innerHTML = "   "
+
+    if (obj.value > 18) 
+    document.getElementById("text").value = " Usted es mayor de edad"
+
+    if (obj.value < 18)
+    document.getElementById("text").value = " Usted es menor de edad "
+ }
+
