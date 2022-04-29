38696addd40cf3c2fcf7ba560a92f1d73d6b4441
raidercl <103221925+raidercl@users.noreply.github.com>
2022-04-29 15:35:00 -0400

    Add files via upload

--- miarchivo.js
+++ miarchivo.js
@@ -3,17 +3,31 @@ console.log("Hola mundo");
  let variable = 45;
  const constante = 10;
  console.log(variable,constante);
+ let edad = document.getElementById("edad").value
 
  function react() {
     let obj = document.getElementById("react");
     console.log(obj.value)
 
     document.getElementById("text").innerHTML = "   "
-
-    if (obj.value > 18) 
+      //// Validacion edad
+    if (obj.value > 17) {
     document.getElementById("text").value = " Usted es mayor de edad"
 
-    if (obj.value < 18)
-    document.getElementById("text").value = " Usted es menor de edad "
- }
+   }else{ (obj.value < 18)
+    document.getElementById
+    ("text").value = " Usted es menor de edad "
+    }
+    let genero = document.getElementById("genero");
+    console.log("Arreglo.length"+arreglo.length)
+
+    for ( let x = 0; x < arreglo.length; x++) {
+       console.log(arreglo[x])
+    }
+
+}
+
+{
+
+}
 
